dbe057b717e41dcd33387ea8eebaa0b051a314fd
devoperations <chrishayes0130@gmail.com>
2022-06-11 18:53:52 -0700

    CSS

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -41,7 +41,7 @@ export default class SignUpForm extends Component {
     const disable = this.state.password !== this.state.confirm;
     return (
       <div>
-        <div className="form-container">
+        <div className="login-container">
           <form autoComplete="off" onSubmit={this.handleSubmit}>
             <label>Name</label>
             <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
@@ -51,7 +51,9 @@ export default class SignUpForm extends Component {
             <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
             <label>Confirm</label>
             <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+           
             <button type="submit" disabled={disable}>SIGN UP</button>
+           
           </form>
         </div>
         <p className="error-message">&nbsp;{this.state.error}</p>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,10 @@
+import"./HomePage.css"
 export default function HomePage(){
     return(
         
         <div className="homepage">
-        <h1> Good Day Street</h1>
-        
-      </div>
-          
-
-
+          <h1> Good Day Street</h1>
+          <img src = "https://i.imgur.com/X8tZjio.png"/>       
+        </div>
     )
 }
